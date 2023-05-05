f3ac7c2a3240eb9465c1cee1addde52ecb09b86c
Chay Fadida <chayfadida@Chays-MacBook-Pro.local>
2023-05-05 17:10:09 +0300

    first commit

---
+++ CEMS_proj/src/common/ChatIF.java
@@ -0,0 +1,11 @@
+
+package common;
+
+/*
+ * This interface implements the abstract method used to display
+ * objects onto the client or server UIs.
+*/
+public interface ChatIF 
+{
+  public abstract void display(Object message);
+}
