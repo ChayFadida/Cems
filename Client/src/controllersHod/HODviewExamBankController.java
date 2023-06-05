dec1ba3954bec9cde8f69ec4c57a9be1aaf195c8
yoni4600 <128678152+yoni4600@users.noreply.github.com>
2023-06-05 10:37:32 +0300

    all screens connected (#28)

---
+++ Client/src/controllersHod/HODviewExamBankController.java
@@ -0,0 +1,14 @@
+package controllersHod;
+
+import java.awt.TextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class HODviewExamBankController {
+    @FXML
+    private TableView<?> ExamsTable;
+
+    @FXML
+    private TextField LecturerIdText;
+}
