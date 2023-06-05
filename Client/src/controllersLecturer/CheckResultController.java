7f54dd27cf0e252ec37253d06eb2f09614a78c77
TomerLazarovitch7 <128506101+TomerLazarovitch7@users.noreply.github.com>
2023-06-05 10:40:05 +0300

    Lecturer GUI finished. (#29)
    Lecturer GUI finished.
    
    Co-authored-by: ChayFadida <122904042+ChayFadida@users.noreply.github.com>

--- Client/src/controllersLecturer/CheckResultController.java
+++ Client/src/controllersLecturer/CheckResultController.java
@@ -1,8 +1,10 @@
 package controllersLecturer;
 
 import abstractControllers.AbstractController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CheckResultController extends AbstractController{
 
@@ -11,5 +13,22 @@ public class CheckResultController extends AbstractController{
 
     @FXML
     private Button EditGradeButton;
+    
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private Button minimizeButton;
+    
+    @FXML
+    void Minimize(ActionEvent event) {
+    	Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+    @FXML
+    void Close(ActionEvent event) {
+    	System.exit(0);
+    }
 
 }
--- Client/src/controllersLecturer/MyExamBankController.java
+++ Client/src/controllersLecturer/MyExamBankController.java
@@ -1,12 +1,13 @@
 package controllersLecturer;
 
-import abstractControllers.AbstractController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
-public class MyExamBankController extends AbstractController{
+public class MyExamBankController {
 
     @FXML
     private ComboBox<?> CourseComboBox;
@@ -19,5 +20,51 @@ public class MyExamBankController extends AbstractController{
 
     @FXML
     private TableView<?> examTable;
+    
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private Button minimizeButton;
+    
+    @FXML
+    void Minimize(ActionEvent event) {
+    	Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+    @FXML
+    void Close(ActionEvent event) {
+    	System.exit(0);
+    }
+
+    @FXML
+    void DeleteExam(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditExam(ActionEvent event) {
+       	//((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
+    	Stage primaryStage = new Stage();
+    	EditExamController editExamController = new EditExamController();
+    	//need to implement start method in AddNewQuestionController and then -->
+    	editExamController.start(primaryStage);
+    }
 
 }
+
+    
+   /* @FXML
+    void DeleteExam(ActionEvent event) {
+    	//needs implementation
+    }*/
+
+    /*@FXML
+    void EditExam(ActionEvent event) {
+    	//((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
+		Stage primaryStage = new Stage();
+		EditExamController editExamController = new EditExamController();
+		//need to implement start method in AddNewQuestionController and then -->
+		editExamController.start(primaryStage);
+    }*/
