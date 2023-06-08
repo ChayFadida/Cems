8bf71a106a95ddc958f09e04b67ea6a27d48e979
ChayFadida <chayfadida1997@gmail.com>
2023-06-08 12:39:37 +0300

    Clean Git cache

--- Client/src/timer/CountDownObserver.java
+++
@@ -1,6 +0,0 @@
-package timer;
-
-public interface CountDownObserver {
-    void update(int seconds);
-    void timeIsUp();
-}
--- Server/src/logic/ClientConnection.java
+++
@@ -1,5 +0,0 @@
-package logic;
-
-public enum ClientConnection {
-	CONNECTED, DISCONNECTED;
-}
