a489676470650daa48753e6e20a42c1f99a83b7b
yoni4600 <128678152+yoni4600@users.noreply.github.com>
2023-06-12 20:50:35 +0300

    kson

--- Common/src/thirdPart/jsonHandler.java
+++ Common/src/thirdPart/jsonHandler.java
@@ -5,9 +5,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
-public class jsonHandler {
-	
-	public static <K, V> HashMap<K, V> convertJsonToHashMap(String jsonString, Type keyType, Type valueType) {
+public class JsonHandler {
+ 
+    public static <K, V> HashMap<K, V> convertJsonToHashMap(String jsonString, Type keyType, Type valueType) {
         Gson gson = new Gson();
         Type hashMapType = TypeToken.getParameterized(HashMap.class, keyType, valueType).getType();
         if (jsonString == null || jsonString.isEmpty()) {
@@ -25,4 +25,5 @@ public class jsonHandler {
         Type hashMapType = TypeToken.getParameterized(HashMap.class, keyType, valueType).getType();
         return gson.toJson(hashMap, hashMapType);
     }
+
 }
