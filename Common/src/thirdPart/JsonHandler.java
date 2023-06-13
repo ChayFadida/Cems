febdaf57fdeb2cc465211622120bb927db5664a3
TomerLazarovitch7 <tomer.lazarovitch@e.braude.ac.il>
2023-06-13 21:13:30 +0300

    ssssEmec
    ssssEmec

--- Common/src/thirdPart/JsonHandler.java
+++ Common/src/thirdPart/JsonHandler.java
@@ -1,5 +1,4 @@
 package thirdPart;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.lang.reflect.Type;
 import com.google.gson.Gson;
@@ -7,31 +6,10 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 public class JsonHandler {
-	private static HashMap<Class<?>, TypeToken<?>> class_token = new HashMap<>(){{
-		put(String.class,  new TypeToken<ArrayList<String>>() {});
-		put(Integer.class, new TypeToken<ArrayList<Integer>>() {});
-		put(Double.class,  new TypeToken<ArrayList<Double>>(){});
-	}};
-	
-	public static <K, V> HashMap<K, V> convertJsonToHashMap(String jsonString, Type keyType, Type valueType) {
-	    Gson gson = new Gson();
-	    Type hashMapType = TypeToken.getParameterized(HashMap.class, keyType, valueType).getType();
-	    if (jsonString == null || jsonString.isEmpty()) {
-	        return new HashMap<>();
-	    }
-	    try {
-	    	return gson.fromJson(jsonString, hashMapType);
-	    } catch (JsonSyntaxException e) {
-	        return new HashMap<>();
-	    }
-	}
-
-    public static <K, V> HashMap<K, V> convertJsonToHashMap(String jsonString, Type keyType, Type valueType, Type arrayType) {
+ 
+    public static <K, V> HashMap<K, V> convertJsonToHashMap(String jsonString, Type keyType, Type valueType) {
         Gson gson = new Gson();
-        Type hashMapType;
-
-
-        hashMapType = TypeToken.getParameterized(HashMap.class, keyType, class_token.get(arrayType).getType()).getType();        
+        Type hashMapType = TypeToken.getParameterized(HashMap.class, keyType, valueType).getType();
         if (jsonString == null || jsonString.isEmpty()) {
             return new HashMap<>();
         }
@@ -41,6 +19,9 @@ public class JsonHandler {
             return new HashMap<>();
         }
     }
+    
+    
+
     public static <K, V> String convertHashMapToJson(HashMap<K, V> hashMap, Type keyType, Type valueType) {
         Gson gson = new Gson();
         Type hashMapType = TypeToken.getParameterized(HashMap.class, keyType, valueType).getType();
