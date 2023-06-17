de37552443c044375e40ffe4b34b5d2c69cf8375
eyalCohenB <128489786+eyalCohenB@users.noreply.github.com>
2023-06-17 18:42:51 +0300

    Lecturer Check Exam Result Fully Functioning :) (#89)
    Co-authored-by: ChayFadida <122904042+ChayFadida@users.noreply.github.com>

--- Client/src/controllersHod/HODReasonsRequestController.java
+++ Client/src/controllersHod/HODReasonsRequestController.java
@@ -1,18 +1,12 @@
 package controllersHod;
 
-import java.io.IOException;
-
 import abstractControllers.AbstractController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 /**
@@ -20,6 +14,7 @@ import javafx.stage.StageStyle;
  * In this controller the HOD will see all of his reasons for changing the time duration.
  * Extends AbstractController.
  */
+
 public class HODReasonsRequestController extends AbstractController{
 	Image img = new Image("Images/exit.png");
     @FXML
@@ -52,6 +47,5 @@ public class HODReasonsRequestController extends AbstractController{
 		Stage currentStage = (Stage) btnExit.getScene().getWindow();
         currentStage.close();
 	}
-
 }
 
