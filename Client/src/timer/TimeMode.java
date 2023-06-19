ea937c69a08ba89658bdd4e64b03814fea69b1a0
eyalCohenB <128489786+eyalCohenB@users.noreply.github.com>
2023-06-20 01:04:05 +0300

    documented Timer package at Client proj (#128)
    Co-authored-by: ChayFadida <122904042+ChayFadida@users.noreply.github.com>

--- Client/src/timer/TimeMode.java
+++ Client/src/timer/TimeMode.java
@@ -1,28 +1,50 @@
 package timer;
-
+/*
+ * represents a managing tool for given amount of time
+ * and manages the way it returns as needed per usage
+ */
 public class TimeMode {
 
     private int seconds;
-
+    /**
+	 * Constructor for TimeMode.
+	 * @param int minutes.
+	 */
     public TimeMode(int minutes) {
         seconds = minutes * 60;
     }
-
+    /**
+	 * method for returning given time as seconds.
+	 * @param non.
+	 */
     public int getSeconds() {
         return seconds;
     }
-
+    /**
+	 * method for returning given time as minutes.
+	 * @param non.
+	 */
     public int getMinutes() {
         return seconds / 60;
     }
-    
+    /**
+	 * method for returning given time as hours.
+	 * @param non.
+	 */
     public int getHours() {
         return seconds / 3600;
     }
-
+    /**
+	 * method for setting current time as given minutes.
+	 * @param int minutes.
+	 */
     public void setMinutes(int minutes) {
         seconds = minutes * 60;
     }
+    /**
+	 * method for setting current time as given hours.
+	 * @param int minutes.
+	 */
     public void setHours(int minutes) {
         seconds = minutes * 3600;
     }
