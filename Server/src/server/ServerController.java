15bc9bd10e849fec540f60029a3c5b88e994bdab
yoni4600 <128678152+yoni4600@users.noreply.github.com>
2023-06-19 22:10:27 +0300

    Yoni/fixchartbar (#123)
    Co-authored-by: itamar-kraus <127882088+itamar-kraus@users.noreply.github.com>

--- Server/src/server/ServerController.java
+++ Server/src/server/ServerController.java
@@ -108,7 +108,7 @@ public class ServerController  {
 //		}
 		HashMap<String, String> db_info_temp = new HashMap<>() {{
 			put("ip","localhost");
-			put("password", "EyalMySql");
+			put("password", "Aa123456");
 			put("username", "root");
 			put("scheme", "sys");
 			put("port", "8000");
